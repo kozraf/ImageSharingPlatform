9c1afba71ff1a6f5db2042e1fd603b6498f73dcb
kozraf <kozraf@gmail.com>
2023-09-24 22:16:54 +0100

    adjusting main.tf and variables.tf

--- variables.tf
+++ variables.tf
@@ -2,14 +2,6 @@ variable "region" {
   type = string
 }
 
-variable "access_key" {
-  type = string
-}
-
-variable "secret_key" {
-  type = string
-}
-
 variable "S3_BUCKET" {
   description = "The name of the S3 bucket"
   default     = "s3-image-processing-8a73"  #
