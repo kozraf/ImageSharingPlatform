3a859eebc3da5307e903b29b1d477f8d704983ef
kozraf <kozraf@gmail.com>
2023-09-24 21:55:56 +0100

    updating main.tf for remote backend s3 bucket name

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "s3bucket4tfstate--fast-pegasus"
+    bucket         = "s3bucket4tfstate--classic-leech"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tfstate_lock"
